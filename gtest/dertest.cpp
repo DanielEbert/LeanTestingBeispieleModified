d9cff96c7541b77cf87be43006979222e80d12ce
Daniel Ebert <iuq@web.de>
2020-12-18 13:53:02 -0800

    Chapter gtest

--- gtest/dertest.cpp
+++ gtest/dertest.cpp
@@ -15,15 +15,19 @@ TEST(Pruefling_Test, 3a_positiveZahl) {
 }
 TEST(Pruefling_Test, 3b_positiveZahl) {
   pruefling p(10.0);
-  EXPECT_DOUBLE_EQ(1000.0, p.attributquadrat());
+  EXPECT_DOUBLE_EQ(100.0, p.attributquadrat());
 }
 
 TEST(Pruefling_Test, 4_ULP_test) {
   pruefling p(2.5);
-  EXPECT_DOUBLE_EQ(6.25 + 1e-15, p.attributquadrat());
-  if(6.25 + 1e-15 != 6.25) {
+  EXPECT_DOUBLE_EQ(6.25, p.attributquadrat());
+  if(6.25 != 6.25) {
     std::cout << "UNGLEICH!\n";
   }
 }
 
+TEST(Pruefling_Test, 4_negativeZahl) {
+  pruefling p(-10.0);
+  EXPECT_DOUBLE_EQ(100.0, p.attributquadrat());
+}
 // .. usw.
